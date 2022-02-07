62fb65269d42bf485b8a7038c5e7af988dd56627
Amirhossein Shahabnia <amirhosein.shahabnia@gmail.com>
2022-02-07 12:37:26 -0800

    adds the product selection section

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ window.addEventListener('load', () => {
   var imgWidth = batchImg.clientWidth;
   var visibleWidth = body.clientWidth - body.clientWidth / 10;
 
+  // Product img vars
+  // const prodFullImg = document.getElementById('prod-full-img');
+  const mtiFull = document.getElementById('mti2-full');
+  const coreFull = document.getElementById('core-full');
+  const mtzFull = document.getElementById('mtz-full');
+
   rightArrow.addEventListener('click', () => {
     if (left >= -(imgWidth - visibleWidth)) {
-      offset -= 250;
+      offset -= 333;
       batchImg.style.left = offset + 'px';
     }
     left = parseInt(batchImg.style.left.split('px')[0]);
@@ -27,7 +33,7 @@ window.addEventListener('load', () => {
     }
 
     if (left < 0) {
-      offset += 250;
+      offset += 333;
       batchImg.style.left = offset + 'px';
     }
     left = parseInt(batchImg.style.left.split('px')[0]);
@@ -86,6 +92,38 @@ window.addEventListener('load', () => {
     pauseOnHover: true,
     arrows: true,
     centerMode: true,
+  });
+
+  // Prod Title Clicks
+  document.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('prod-title')) return;
+
+    event.target.classList.add('selected-main');
+
+    if (event.target.classList.contains('mti-selector')) {
+      mtiFull.classList.add('img-selected');
+      coreFull.classList.remove('img-selected');
+      mtzFull.classList.remove('img-selected');
+    }
+
+    if (event.target.classList.contains('core-selector')) {
+      mtiFull.classList.remove('img-selected');
+      coreFull.classList.add('img-selected');
+      mtzFull.classList.remove('img-selected');
+    }
+
+    if (event.target.classList.contains('mtz-selector')) {
+      mtiFull.classList.remove('img-selected');
+      coreFull.classList.remove('img-selected');
+      mtzFull.classList.add('img-selected');
+    }
+
+    var elems = document.querySelectorAll('.prod-title');
+
+    elems.forEach((element) => {
+      if (element === event.target) return;
+      element.classList.remove('selected-main');
+    });
   });
 
   // var imgArray = [
